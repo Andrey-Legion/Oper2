792d75495187a38d9fa5077a6b826448ea7379d1
Andrey-Legion <andrey.legion@gmail.com>
2022-10-09 22:55:16 +0300

    ДЗ Условные операторы Часть2

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Task1
+        byte clientOS=1;
+        if (clientOS==0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Аndroid по ссылке");
+        }
+
+        //Task2
+        //Ниже  использовал вложенный оператор ветвления,не смог придумать,как обойтись без него, может через логического оператора..
+        byte clientOS1=0;
+        short clientDeviceYear=2015;
+        if (clientOS1==0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear<2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS1==1) {
+            System.out.println("Установите версию приложения для Аndroid по ссылке");
+        }
+            if (clientDeviceYear<2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
+            //Task3
+        //Не возвращает результат почему-то
+        int year=2021;
+        if (year == year / 4 && year == year / 400) {
+            System.out.println(year + " является високосным.");
+
+        } else if (year==year/100) {
+            System.out.println(year+" год не является високосным");
+        }
+
+        //Task4
+        byte deliveryDistance=95, day=1;
+        if (deliveryDistance<20) {
+            System.out.println("Потребуется дней: "+day);
+                if (deliveryDistance>=20 && deliveryDistance<60) {
+                    System.out.println("Потребуется дней: " + (day + 1));
+                } if (deliveryDistance>=60 && deliveryDistance<100) {
+                        System.out.println("Потребуется дней: "+(day+2));
+                    }
+            } else {
+                System.out.println("Нет доставки");
+            }
+        }
+
+        //Task5
+        byte monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default;
+                System.out.println("Не существует")
+    }
+}
